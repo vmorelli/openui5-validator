47417077d88371be8088bc249e7b3e4396a2718c
Morelli <vmorelli@wheelhub.es>
2018-12-20 18:02:38 +0100

    test injector

--- src/openui5/validator/thirdparty/ajv-i18n/localize/index.js
+++ src/openui5/validator/thirdparty/ajv-i18n/localize/index.js
@@ -1,32 +1,51 @@
-;(function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define(factory) :
-    global.ajvI18n = factory()
-}(this, (function () {
+// ;(function (global, factory) {
+//     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+//     typeof define === 'function' && define.amd ? define(factory) :
+//     global.ajvI18n = factory()
+// }(this, (function () {
 
-'use strict';
+// 'use strict';
 
-var ajvI18n = {
-    en: require('./en'),
-    ar: require('./ar'),
-    cz: require('./cz'),
-    de: require('./de'),
-    es: require('./es'),
-    fr: require('./fr'),
-    hu: require('./hu'),
-    it: require('./it'),
-    ja: require('./ja'),
-    nb: require('./nb'),
-    pl: require('./pl'),
-    'pt-BR': require('./pt-BR'),
-    ru: require('./ru'),
-    sk: require('./sk'),
-    sv: require('./sv'),
-    th: require('./th'),
-    zh: require('./zh'),
-    'zh-TW': require('./zh-TW'),
-};
+// var ajvI18n = {
+//     en: require('./en'),
+//     ar: require('./ar'),
+//     cz: require('./cz'),
+//     de: require('./de'),
+//     es: require('./es'),
+//     fr: require('./fr'),
+//     hu: require('./hu'),
+//     it: require('./it'),
+//     ja: require('./ja'),
+//     nb: require('./nb'),
+//     pl: require('./pl'),
+//     'pt-BR': require('./pt-BR'),
+//     ru: require('./ru'),
+//     sk: require('./sk'),
+//     sv: require('./sv'),
+//     th: require('./th'),
+//     zh: require('./zh'),
+//     'zh-TW': require('./zh-TW'),
+// };
 
-return ajvI18n;
+// return ajvI18n;
 
-})));
+// })));
+
+
+/*!
+ * ${copyright}
+ */
+
+sap.ui.define([
+    './es/index'
+],
+	function(es) {
+	"use strict";
+
+	var ajvI18n = {
+        es: es
+    };
+
+	return ajvI18n;
+
+}, /* bExport= */ true);
